7253c8e3b69139836112df8baaadb4fc4113fc46
Stephen Olson <stephenolson@gmail.com>
2018-11-21 20:54:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if @status = "complete"
+    if @status == "complete"
       @receiver.balance = @receiver.balance - @amount
       @sender.balance = @sender.balance + @amount
       @status = "reversed"
